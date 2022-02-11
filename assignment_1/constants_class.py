3bc278278228533a694246783ca7f343ccb6ee26
lukas_welzel <lukas.leonard.welzel@gmail.com>
2022-02-11 18:38:05 +0100

    Update particle class and framework

---
+++ assignment_1/constants_class.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+class Constants(object):
+    """
+    Virtual Class to keep constants stored somewhere save.
+    Very cool.
+    """
+
+    sbk = 5.670374419e-8    # W/m2/K4       # Stephan-Boltzmann constant
+    bk = 1.38064852e-23     # m2*kg/s2/K1   # Boltzmann constant
--- assignment_1/particle_class.py
+++ assignment_1/particle_class.py
@@ -1,10 +1,27 @@
 import numpy as np
 
-class Particle(object):
+class Particle(object, n_particles=None):
     """
     Class to keep track of particles
     """
-    def __init__(self, n_steps, n_dim, pos, vel, acc,
+    # TODO: should the class keep and updated version of the particle positions for each step so we dont need to
+    #  access the individual particles each tick? Below implementation requires all particles to be initialized
+
+    # global_position_matrix = []
+
+    # TODO: maybe this is a bad idea but Id like to avoid passing around objects all the time.
+    #  So what I suggest is that we store the instances in the class itself (at least as references).
+    #  This is probably really stupid and is gonna bite us in the butt later but for now I dont see how it could.
+    #  Except if there is a lot of particles of course but then we are screwed anyway :)
+    #  Has the same(-ish) problem to the one above, but is nicer
+
+    # ok stupid solution first, max number of particles = 10000
+    # this also makes mp really impossible, oh well...
+    __max_particles = 10000 # should be passed to class
+    __instances__ = np.full(__max_particles, fill_value=np.nan, dtype=object)
+
+    def __init__(self, n_steps, n_dim,
+                 initial_pos, initial_vel, initial_acc,
                  mass, name="",
                  **kwargs):
         """
@@ -34,3 +51,27 @@ class Particle(object):
         pos = np.full(shape=shape, fill_value=np.nan)
         vel = np.full(shape=shape, fill_value=np.nan)
         acc = np.full(shape=shape, fill_value=np.nan)
+
+        # below is if we use a set (add particles separately)
+        # self.__class__.__instances__.add(self)
+
+    def propagate(self):
+        return
+
+
+    @classmethod
+    def get_instances(cls):
+        return cls.__instances__
+
+    @classmethod
+    def get_n_active_particles(cls):
+        nans = np.isnan(cls.__instances__)
+        return np.size(nans) - np.count_nonzero(nans)
+
+    @staticmethod
+    def get_shortest_distance(pos1, pos2):
+        return
+
+
+
+
