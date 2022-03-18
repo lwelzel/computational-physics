e69d510f9e30644388b4f3428cb8fad04c70c7b3
Lukas Welzel <lukas.leonard.welzel@gmail.com>
2022-03-18 16:16:00 +0100

    Clean up of main

--- assignment_1/static_plotting.py
+++ assignment_1/static_plotting.py
@@ -71,44 +71,45 @@ def read_h5_data(loc, ):
 
 
 def plotly_3d_static(pos):
-	#df = px.data.iris()
-    f = open('norm_boxlength.txt','r')
+    # df = px.data.iris()
+    f = open('norm_boxlength.txt', 'r')
     length = float(f.read())
     f.close
-    half_len = length/2
+    half_len = length / 2
 
-    #add bounding box:
-    edge1=np.linspace(-half_len,half_len, 50)
-    edge2=np.linspace(-half_len,half_len, 50)
-    edge1,edge2=np.meshgrid(edge1,edge2)
-    
-    plane=half_len*np.ones(edge1.shape)
+    # add bounding box:
+    edge1 = np.linspace(-half_len, half_len, 50)
+    edge2 = np.linspace(-half_len, half_len, 50)
+    edge1, edge2 = np.meshgrid(edge1, edge2)
 
-    plane_color=[[0.0, 'rgb(200,200,200)'], [1.0, 'rgb(200,200,200)']]
+    plane = half_len * np.ones(edge1.shape)
+
+    plane_color = [[0.0, 'rgb(200,200,200)'], [1.0, 'rgb(200,200,200)']]
     opacity = .25
 
-    zpos=dict(type='surface', x=edge1, y=edge2, z=plane, opacity = opacity, colorscale=plane_color, showscale=False)
-    zneg=dict(type='surface', x=edge1, y=edge2, z=-plane, opacity = opacity, colorscale=plane_color, showscale=False)
-    xpos=dict(type='surface', x=plane, y=edge2, z=edge1, opacity = opacity, colorscale=plane_color, showscale=False)
-    xneg=dict(type='surface', x=-plane, y=edge2, z=edge1, opacity = opacity, colorscale=plane_color, showscale=False)
-    ypos=dict(type='surface', x=edge1, y=plane, z=edge2, opacity = opacity, colorscale=plane_color, showscale=False)
-    yneg=dict(type='surface', x=edge1, y=-plane, z=edge2, opacity = opacity, colorscale=plane_color, showscale=False)
+    zpos = dict(type='surface', x=edge1, y=edge2, z=plane, opacity=opacity, colorscale=plane_color, showscale=False)
+    zneg = dict(type='surface', x=edge1, y=edge2, z=-plane, opacity=opacity, colorscale=plane_color, showscale=False)
+    xpos = dict(type='surface', x=plane, y=edge2, z=edge1, opacity=opacity, colorscale=plane_color, showscale=False)
+    xneg = dict(type='surface', x=-plane, y=edge2, z=edge1, opacity=opacity, colorscale=plane_color, showscale=False)
+    ypos = dict(type='surface', x=edge1, y=plane, z=edge2, opacity=opacity, colorscale=plane_color, showscale=False)
+    yneg = dict(type='surface', x=edge1, y=-plane, z=edge2, opacity=opacity, colorscale=plane_color, showscale=False)
 
-    fig = go.Figure(data=[xpos,xneg,ypos,yneg,zpos,zneg])
+    fig = go.Figure(data=[xpos, xneg, ypos, yneg, zpos, zneg])
 
     size = np.ones(len(pos[0]))
     size[-1] = 5
     size *= 2
 
     for idx in range(pos.shape[0]):
-        df=pd.DataFrame(pos[idx])
+        df = pd.DataFrame(pos[idx])
         df.columns = ['x', 'y', 'z']
-        curr_atom=go.Scatter3d(x=df['x'], y=df['y'], z=df['z'], mode='markers', marker=dict(size=size, line=dict(width=0)))
+        curr_atom = go.Scatter3d(x=df['x'], y=df['y'], z=df['z'], mode='markers',
+                                 marker=dict(size=size, line=dict(width=0)))
         fig.add_trace(curr_atom)
 
-    fig.update_layout(showlegend=False) 
+    fig.update_layout(showlegend=False)
     fig.show()
-    
+
     return
 
 
@@ -122,36 +123,19 @@ def mpl_strict_2d_static(pos):
         n_cols = 1
         n_plots = [0]
     elif pos.shape[2] == 3:
-<<<<<<< HEAD
         n_rows = 2
         n_cols = 2
         n_plots = [0, 2, 3]
 
-    fig, axs = plt.subplots(nrows=n_rows, ncols=n_cols,
-                            constrained_layout=True, subplot_kw={'aspect': 1},
-                            # sharex=True, sharey=True,
-                            figsize=(12, 12))
+    fig, axes = plt.subplots(nrows=n_rows, ncols=n_cols,
+                             constrained_layout=True, subplot_kw={'aspect': 1},
+                             # sharex=True, sharey=True,
+                             figsize=(7, 7))
     # structure  is (particles, steps, n_dims, 1)
 
-    axs[0, 1].axis('off')
-    axes = axs.flatten()
-=======
-    	n_rows=2
-    	n_cols=2
-    	n_plots=[0,2,3]
-	
-    fig, axes = plt.subplots(nrows=n_rows, ncols=n_cols,
-                           constrained_layout=True, subplot_kw={'aspect': 1},
-                           # sharex=True, sharey=True,
-                           figsize=(7, 7))
-    # structure  is (particles, steps, n_dims, 1)
-    
-    
     if pos.shape[2] == 3:
-    	axes[0,1].axis('off')
-    	axes=axes.flatten()
-    	
->>>>>>> dev_lukas
+        axes[0, 1].axis('off')
+        axes = axes.flatten()
 
     # sizes of markers, last position is fat
     size = np.ones(len(pos[0]))
@@ -173,7 +157,6 @@ def mpl_strict_2d_static(pos):
     # for Plot labels
     label_format = '{:.0f}'
     n_ticks = 5
-<<<<<<< HEAD
 
     # For MCI box
     f = open('norm_boxlength.txt', 'r')
@@ -226,70 +209,6 @@ def mpl_strict_2d_static(pos):
         # ANNOTATIONS
         ax.legend()
 
-=======
-    
-    #For MCI box
-    f = open('norm_boxlength.txt','r')
-    length = float(f.read())
-    #print(length)
-    f.close
-    max = length * 1.05
-    
-    for plt_idx in n_plots:
-    	if pos.shape[2] == 3:
-    		ax=axes[plt_idx]
-    	else:
-    		ax=axes
-    	if plt_idx == 0: #XY Plane
-    		for i, (particle, color) in enumerate(zip(pos, colors)):
-    			ax.scatter(*np.reshape(particle[:,[0,1]],newshape=(len(particle), -1)).T, s=size, c=color, alpha=alpha)
-    			if len(n_plots) > 1:
-    				ax.set_title('Projection onto XY Plane')
-    				ax.set_xlabel(r'$\frac{r_{x}}{\sigma}$ [-]')
-    				ax.set_ylabel(r'$\frac{r_{y}}{\sigma}$ [-]')
-    	
-    	if plt_idx == 2: #XZ Plane
-        	for i, (particle, color) in enumerate(zip(pos, colors)):
-        		ax.scatter(*np.reshape(particle[:,[0,2]],newshape=(len(particle), -1)).T, s=size, c=color, alpha=alpha)
-        		ax.set_title('Projection onto XZ Plane')
-        		ax.set_xlabel(r'$\frac{r_{x}}{\sigma}$ [-]')
-        		ax.set_ylabel(r'$\frac{r_{z}}{\sigma}$ [-]')
-    	
-    	if plt_idx == 3: #YZ Plane
-    		for i, (particle, color) in enumerate(zip(pos, colors)):
-    			ax.scatter(*np.reshape(particle[:,[1,2]],newshape=(len(particle), -1)).T, s=size, c=color, alpha=alpha)
-    			ax.set_title('Projection onto YZ Plane')
-    			ax.set_xlabel(r'$\frac{r_{y}}{\sigma}$ [-]')
-    			ax.set_ylabel(r'$\frac{r_{z}}{\sigma}$ [-]')
-    			
-    	# AXIS TICK LABELS
-    	#max = np.power(10, np.ceil(np.log10(np.max(pos)))) * 1.05  # for adding box margin
-    	ax.set_xlim(-max, max)
-    	ax.set_ylim(-max, max)
-        
-    	rect = mpl.patches.Rectangle((-length/2, -length/2), length, length, linewidth=1, edgecolor='k', facecolor='none', label="Bounding MCI Box")
-    	
-    	#Add the patch to the Axes
-    	ax.add_patch(rect)
-    	
-    	#ANNOTATIONS
-    	ax.legend()
-
-        
-    	
-    	
-    	# for i, (particle, color) in enumerate(zip(pos, colors)):
-#         	ax.scatter(*np.reshape(particle,
-#                                newshape=(len(particle), -1)).T,
-#                    s=size,
-#                    c=color,
-#                    alpha=alpha,
-#                    # label=f"P{i:03}"
-#                    )
-
-
-    #fig.suptitle(f'MD Simulation', fontsize=20, weight="bold")
->>>>>>> dev_lukas
     fig.suptitle(f'Argon: LJ-Potential\n' r'$by~L. Welzel~and~C. Slaughter$', fontsize=20, weight="bold")
 
     plt.savefig("MD_Argon_strict_2D.png", dpi=300, format="png", metadata=None,
@@ -308,7 +227,7 @@ def mpl_plot_stats(sim):
 
 def main():
     pos = read_h5_data(Path("MD_simulation.h5"))
-    #mpl_strict_2d_static(pos)
+    # mpl_strict_2d_static(pos)
     plotly_3d_static(pos)
 
 
