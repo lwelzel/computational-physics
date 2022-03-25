c5000f7569e5721f53d187d4c3fd20c98045ad7c
lukas_welzel <lukas.leonard.welzel@gmail.com>
2022-03-25 13:30:36 +0100

    Initial commit A2

---
+++ assignment_2/lsing_class.py
@@ -0,0 +1,25 @@
+import numpy as np
+_rng = np.random.default_rng()
+
+class Lsing(np.empty):
+    """
+
+    """
+
+    def __init__(self, shape=(50, 50)):
+        super(Lsing, self).__init__(shape, dtype=bool)
+
+
+
+    @staticmethod
+    def setup_lsing(n=50, s=1):
+
+        return
+
+    @staticmethod
+    def run_lsing():
+        rng = np.random.default_rng
+
+
+if __name__ == "__main__":
+    lsing = Lsing()
---
+++ assignment_2/lsing_main.py
@@ -0,0 +1,22 @@
+import numpy as np
+_rng = np.random.default_rng()
+
+
+def get_initial_grid(n=50):
+    return _rng.choice([True, False], shape=(n, n), dtype=bool)
+
+def
+
+def lsing():
+    rng = np.random.default_rng
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    lsing()
